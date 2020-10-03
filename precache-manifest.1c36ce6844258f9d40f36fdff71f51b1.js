2178ca5b2d157cf91a56e5ac816038ea69f92bfc
webala <webala1001@gmail.com>
2020-10-03 10:56:48 +0300

    Updates

---
+++ precache-manifest.1c36ce6844258f9d40f36fdff71f51b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e312f3fbe5fb28146fb3a0ef2d57bb2",
+    "url": "/creative_crew_site/index.html"
+  },
+  {
+    "revision": "b93b3c4aa71d24b9f8c1",
+    "url": "/creative_crew_site/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4f58e2fdef2a4b7e4a2a",
+    "url": "/creative_crew_site/static/css/main.2d0a5a28.chunk.css"
+  },
+  {
+    "revision": "b93b3c4aa71d24b9f8c1",
+    "url": "/creative_crew_site/static/js/2.29ff0c51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/creative_crew_site/static/js/2.29ff0c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f58e2fdef2a4b7e4a2a",
+    "url": "/creative_crew_site/static/js/main.940ef8c3.chunk.js"
+  },
+  {
+    "revision": "d90f457c5acf88f168c8",
+    "url": "/creative_crew_site/static/js/runtime-main.ec3ef369.js"
+  },
+  {
+    "revision": "7fbe848e1b49cd2b1a66a34a20f4e193",
+    "url": "/creative_crew_site/static/media/creativeimg.7fbe848e.jpeg"
+  },
+  {
+    "revision": "452eecbd001223bf306363f481337c10",
+    "url": "/creative_crew_site/static/media/logo.452eecbd.jpg"
+  }
+]);
