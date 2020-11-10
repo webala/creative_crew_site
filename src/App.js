6122b21d4f9349775f37799ad208c480e43aba6d
= <=>
2020-11-10 23:12:45 +0300

    Router changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import VideosPage from './components/videos'
 import Navbar from './components/navbar';
 
-import {BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link, Redirect, HashRouter } from 'react-router-dom';
 import notFoundPage from './components/404';
 
 
@@ -13,14 +13,14 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Router>
+      <HashRouter>
         <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/404" component={notFoundPage} />
         <Route exact path="/videos" component={VideosPage} />
         <Redirect to="/404" />
         </Switch>
-      </Router>
+      </HashRouter>
      
     </div>
   );
